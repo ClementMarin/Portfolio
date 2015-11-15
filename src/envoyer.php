fe41f815dfc895544119bdf84def3b3276cba670
Marin Clément <cle.marin.38@gmail.com>
2015-11-15 21:56:08 +0100

    Ajout du Menu (soucis de conversion mobile)

--- src/envoyer.php
+++ src/envoyer.php
@@ -8,20 +8,20 @@ $monmessage=$_POST['monmessage'];
 
 $to = "contact@clement-marin.xyz";
 $subject = $object;
-$message = " Name: " . $name . "\r\n Email: " . $email . "\r\n Monmessage: " . $monmessage;
+$message = " Name: " . $name . "\r\n Email: " . $email . "\r\n Message: " . $monmessage;
 
 
-$from = "Blueseodesign";
+$from = $email;
 $headers = "From:" . $from . "\r\n";
 $headers .= "Content-type: text/plain; charset=UTF-8" . "\r\n";
 
 if(@mail($to,$subject,$message,$headers))
 {
-  //print "<script>document.location.href='http://mon-site/merci.htm';</script>";
-  // Created by Future Tutorials
+  print "<script>document.location.href='http://clement-marin.xyz/#contact';</script>";
+  echo"<script language=\"javascript\">";
+  echo"alert(\"Message envoyé à contact@clement-marin.xyz\")";
+  echo "</script>";
 }else{
   echo "Erreur! Veuillez Remplir les Champs Requis.";
 }
-
-
 ?>
